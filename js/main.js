6f00890c70148e2340e25a2584b9f43c129e6e55
OnlyDropped <asheburenkov@yandex.ru>
2021-12-03 23:43:04 +0300

    added media 375px

--- js/main.js
+++ js/main.js
@@ -8,6 +8,33 @@ $('.slider').slick({
   autoplaySpeed: 500,
   infinite: false,
   draggable: false,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        //slidesToShow: 3,
+        //slidesToScroll: 3,
+        //infinite: true,
+        //dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        //slidesToShow: 2,
+        //slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 376,
+      settings: {
+        autoplay: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 
 
@@ -19,4 +46,16 @@ $('.tabs-wrapper').each(function() {
 		ths.find('.tab').removeClass('active').eq($(this).index()).addClass('active');
 		ths.find('.tab-item').hide().eq($(this).index()).fadeIn()
 	}).eq(0).addClass('active');
+});
+
+
+
+document.querySelector('.burger').addEventListener('click', function(){
+  document.querySelector('.burger span').classList.toggle('active');
+  document.querySelector('.header-nav__list').classList.toggle("animate");
+})
+
+$(".footer-item__box-title").on("click", function () {
+  $(this).siblings().slideToggle();
+  $(this).toggleClass('active');
 });
